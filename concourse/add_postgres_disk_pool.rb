b45dfabad20e18c1cc35f332637ea33cb5e3d1a6
Nima Kaviani <nkavian@us.ibm.com>
2017-04-27 16:16:57 -0700

    io2 -> io1
    [#134111165]
    
    Signed-off-by: Chris Piraino <cpiraino@pivotal.io>

--- concourse/add_postgres_disk_pool.rb
+++ concourse/add_postgres_disk_pool.rb
@@ -4,7 +4,7 @@ require 'yaml'
 cf_manifest_path = ARGV[0]
 manifest = YAML.load_file cf_manifest_path
 
-disk_pool = {"cloud_properties" => { "type" => "io2", "iops" => 2000 }, "disk_size" => 100000, "name" => "postgres-persistent-disk"}
+disk_pool = {"cloud_properties" => { "type" => "io1", "iops" => 2000 }, "disk_size" => 100000, "name" => "postgres-persistent-disk"}
 manifest["disk_pools"] = [disk_pool]
 
 manifest['jobs'].find {|job| job['name'] == 'postgres_z1' }.delete('persistent_disk')
