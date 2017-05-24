6ee5d6f5bf2b632ad44801cb51a2d5a3ed92aa19
Danny Cao <dcao@us.ibm.com>
2017-05-24 12:32:21 -0700

    Add runner_z1 resource pool for postgres
    * cf-release removed the runner_z1 resource pool we were using for
    time-rotor
    
    Signed-off-by: Nima Kaviani <nkavian@us.ibm.com>

--- concourse/add_postgres_disk_pool.rb
+++ concourse/add_postgres_disk_pool.rb
@@ -1,10 +1,41 @@
 #!/usr/bin/env ruby
 
 require 'yaml'
+
 cf_manifest_path = ARGV[0]
 manifest = YAML.load_file cf_manifest_path
 
-disk_pool = {"cloud_properties" => { "type" => "io1", "iops" => 2000 }, "disk_size" => 100000, "name" => "postgres-persistent-disk"}
+copy_obj = manifest["resource_pools"][0]
+
+resource_pool =
+{
+  "cloud_properties" =>
+   {
+     "instance_type" =>  "m3.large",
+     "ephemeral_disk" =>  {
+      "size" =>  102400,
+      "type" =>  "gp2"
+     },
+    "availability_zone" =>  "us-east-1a"
+   },
+ "name" => "runner_z1",
+ "env" => copy_obj["env"],
+ "stemcell" => copy_obj["stemcell"],
+ "network" => "cf1"
+}
+
+manifest["resource_pools"].push(resource_pool)
+
+disk_pool =
+{
+  "cloud_properties" =>
+  {
+    "type" => "io1",
+    "iops" => 2000
+  },
+  "disk_size" => 100000,
+  "name" => "postgres-persistent-disk"
+}
 manifest["disk_pools"] = [disk_pool]
 
 manifest['jobs'].find {|job| job['name'] == 'postgres_z1' }.delete('persistent_disk')
