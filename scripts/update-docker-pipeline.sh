acede8abf2d972241e1d3ba8785567df9aafc385
Nick Wei <nwei@pivotal.io>
2019-08-16 13:34:41 -0400

    fix docker update pipeline script

--- scripts/update-docker-pipeline.sh
+++ scripts/update-docker-pipeline.sh
@@ -2,4 +2,10 @@
 
 set -ex
 
-fly -t ${DIEGO_CI_TARGET:-ci} sp -p main -c diego-pipeline.yml --load-vars-from <(lpass show --notes "diego-pipeline-secrets")
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+lpass status 2>&1 > /dev/null;
+if [[ $? -eq 0 ]]; then
+  fly -t ${DIEGO_CI_TARGET:-ci} sp -p docker-images -c $DIR/../pipelines/docker-pipeline.yml --load-vars-from <(lpass show --notes "diego-pipeline-secrets")
+else
+  echo "Login to lastpass: 'lpass login ...'";
+fi
