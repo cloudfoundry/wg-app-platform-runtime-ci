993ca70720f91a19bead2cb99659ff3310fbe65d
John Shahid <jshahid@pivotal.io>
2020-04-10 20:41:39 +0000

    Pass extra arguments to fly
    Authored-by: John Shahid <jshahid@pivotal.io>

--- scripts/update-diego-pipeline.sh
+++ scripts/update-diego-pipeline.sh
@@ -3,8 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 lpass status 2>&1 > /dev/null;
 if [[ $? -eq 0 ]]; then
-  fly -t ${DIEGO_CI_TARGET:-ci} sp -p main -c $DIR/../pipelines/diego-pipeline.yml --load-vars-from <(lpass show --notes "diego-pipeline-secrets")
+  fly -t ${DIEGO_CI_TARGET:-ci} sp -p main -c $DIR/../pipelines/diego-pipeline.yml --load-vars-from <(lpass show --notes "diego-pipeline-secrets") "$@"
 else
   echo "Login to lastpass: 'lpass login ...'";
 fi
-
