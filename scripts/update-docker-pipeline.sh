55f77e54c0d940e06db546281ad33ca6b6524adc
Sunjay Bhatia <sbhatia@pivotal.io>
2020-06-15 18:09:35 +0000

    Add find fly binary logic to docker pipeline update script
    Co-authored-by: Mark DeLillo <mdelillo@pivotal.io>

--- scripts/update-docker-pipeline.sh
+++ scripts/update-docker-pipeline.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-set -ex
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+FLY_EXE=$(which fly)
+if type -p "fly-diego-ci" > /dev/null; then
+  FLY_EXE=$(which fly-diego-ci)
+fi
 lpass status 2>&1 > /dev/null;
 if [[ $? -eq 0 ]]; then
-  fly -t ${DIEGO_CI_TARGET:-ci} sp -p docker-images -c $DIR/../pipelines/docker-pipeline.yml --load-vars-from <(lpass show --notes "diego-pipeline-secrets")
+  "$FLY_EXE" -t ${DIEGO_CI_TARGET:-ci} sp -p docker-images -c $DIR/../pipelines/docker-pipeline.yml --load-vars-from <(lpass show --notes "diego-pipeline-secrets")
 else
   echo "Login to lastpass: 'lpass login ...'";
 fi
