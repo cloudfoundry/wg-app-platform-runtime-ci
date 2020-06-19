b6d76eccb6c706cb16f1693d572d6af2ea2aa71d
John Shahid <jshahid@pivotal.io>
2020-06-19 20:17:28 +0000

    Pass remaining arguments to fly

--- scripts/update-docker-pipeline.sh
+++ scripts/update-docker-pipeline.sh
@@ -7,7 +7,7 @@ if type -p "fly-diego-ci" > /dev/null; then
 fi
 lpass status 2>&1 > /dev/null;
 if [[ $? -eq 0 ]]; then
-  "$FLY_EXE" -t ${DIEGO_CI_TARGET:-ci} sp -p docker-images -c $DIR/../pipelines/docker-pipeline.yml --load-vars-from <(lpass show --notes "diego-pipeline-secrets")
+  "$FLY_EXE" -t ${DIEGO_CI_TARGET:-ci} sp -p temp-docker-images-go1.13.12 -c $DIR/../pipelines/docker-pipeline.yml --load-vars-from <(lpass show --notes "diego-pipeline-secrets") "$@"
 else
   echo "Login to lastpass: 'lpass login ...'";
 fi
