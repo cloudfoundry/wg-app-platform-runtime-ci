3acfad4155539408eed1279cb8964bbd129cf8db
Amin Jamali <ajamali@vmware.com>
2023-06-16 14:35:58 +0000

    Make Rootfs an input for Configure-Gdn

--- garden-runc-release/helpers/configure-binaries.ps1
+++ garden-runc-release/helpers/configure-binaries.ps1
@@ -1,9 +1,14 @@
 function Configure-Gdn
 {
+    Param
+        (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $Rootfs
+        )
     $env:PATH = "C:/var/vcap/bosh/bin;" + $env:PATH
     $tarBin = (get-command tar.exe).source
 
-        $args='server --skip-setup --runtime-plugin={0} --image-plugin="{1}" --image-plugin-extra-arg=--driver-store={2} --image-plugin-extra-arg=--config={3} --network-plugin={4} --network-plugin-extra-arg=--configFile={5} --network-plugin-extra-arg=--log={6} --network-plugin-extra-arg=--debug --bind-ip={7} --bind-port={8} --default-rootfs={9} --nstar-bin={10} --tar-bin={11} --init-bin={12} --depot={13} --log-level=debug' -f $env:WINC_BINARY,$env:GROOT_BINARY,$env:GROOT_IMAGE_STORE,$env:GROOT_CONFIG,$env:WINC_NETWORK_BINARY, $env:WINC_NETWORK_CONFIG,$env:WINC_NETWORK_LOG_FILE, $env:GDN_BIND_IP,$env:GDN_BIND_PORT,$env:WINC_TEST_ROOTFS,$env:NSTAR_BINARY,$tarBin, $env:WINIT_BINARY,$env:GDN_DEPOT_PATH
+        $args='server --skip-setup --runtime-plugin={0} --image-plugin="{1}" --image-plugin-extra-arg=--driver-store={2} --image-plugin-extra-arg=--config={3} --network-plugin={4} --network-plugin-extra-arg=--configFile={5} --network-plugin-extra-arg=--log={6} --network-plugin-extra-arg=--debug --bind-ip={7} --bind-port={8} --default-rootfs={9} --nstar-bin={10} --tar-bin={11} --init-bin={12} --depot={13} --log-level=debug' -f $env:WINC_BINARY,$env:GROOT_BINARY,$env:GROOT_IMAGE_STORE,$env:GROOT_CONFIG,$env:WINC_NETWORK_BINARY, $env:WINC_NETWORK_CONFIG,$env:WINC_NETWORK_LOG_FILE, $env:GDN_BIND_IP,$env:GDN_BIND_PORT,$Rootfs,$env:NSTAR_BINARY,$tarBin, $env:WINIT_BINARY,$env:GDN_DEPOT_PATH
 
         Debug "Start-Process $env:GDN_BINARY with Args: $args"
 
