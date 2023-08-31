00210a8f8ba04e68df8bc7df4a96cb9b0b9e5465
Amin Jamali <ajamali@vmware.com>
2023-08-31 15:24:36 +0000

    Task for running the template tests for a given repo
    Signed-off-by: Brandon Roberson <broberson@vmware.com>

--- shared/tasks/run-tests-templates/task.bash
+++ shared/tasks/run-tests-templates/task.bash
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-set -eux
+set -eEu
+set -o pipefail
 
-pushd repo > /dev/null
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export TASK_NAME="$(basename $THIS_FILE_DIR)"
+source "$THIS_FILE_DIR/../../../shared/helpers/helpers.bash"
+unset THIS_FILE_DIR
+
+function run() {
+    pushd repo > /dev/null
     bundle install
+    debug "Running bundle exec rspec spec $(get_git_remote_name):"
     bundle exec rspec spec
-popd > /dev/null
+    popd > /dev/null
+}
+
+trap 'err_reporter $LINENO' ERR
+run "$@"
