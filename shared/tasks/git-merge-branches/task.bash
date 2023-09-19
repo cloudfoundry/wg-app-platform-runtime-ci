18056b1bcba4239e3311d3a3c0f1a8146a79e2a3
Maria Shaldybin <mariash@vmware.com>
2023-09-19 16:57:23 +0000

    Define branch names in task
    Concourse provides directory with detached head

--- shared/tasks/git-merge-branches/task.bash
+++ shared/tasks/git-merge-branches/task.bash
@@ -13,18 +13,15 @@ function run(){
     git_configure_author
     git_configure_safe_directory
 
-    local onto_branch_name="$(git -C ./onto-branch rev-parse --abbrev-ref HEAD)"
-    local source_branch_name="$(git -C ./source-branch rev-parse --abbrev-ref HEAD)"
-
     git clone ./source-branch ./merged-branch
 
     pushd merged-branch > /dev/null
 
     git remote add local ../onto-branch
     git fetch local
-    git checkout "local/${onto_branch_name}"
+    git checkout "local/${ONTO_BRANCH_NAME}"
 
-    git merge --no-edit "${source_branch_name}"
+    git merge --no-edit "${SOURCE_BRANCH_NAME}"
 
     popd > /dev/null
 }
