541864b8e8c27b543ba1eacdb30470f3536308e0
David Sabeti <sabetid@vmware.com>
2023-09-22 16:40:24 +0000

    Move configure_db helper to shared directory
    Signed-off-by: Amin Jamali <ajamali@vmware.com>

--- shared/helpers/helpers.bash
+++ shared/helpers/helpers.bash
@@ -126,3 +126,32 @@ function err_reporter() {
     fi
 }
 
+function configure_db() {
+  db="$1"
+
+  if [ "${db}" = "postgres" ]; then
+    launchDB="(/postgres-entrypoint.sh postgres &> /var/log/postgres-boot.log) &"
+    testConnection="psql -h localhost -U postgres -c '\conninfo'"
+  elif [ "${db}" = "mysql" ]  || [ "${db}" = "mysql-5.6" ] || [ "${db}" = "mysql8" ]; then
+    launchDB="(MYSQL_ROOT_PASSWORD=password /mysql-entrypoint.sh mysqld &> /var/log/mysql-boot.log) &"
+    testConnection="mysql -h localhost -u root -D mysql -e '\s;' --password='password'"
+  else
+    echo "DB variable not set. The script does not determine which database to use and would fail some tests with errors related to being unable to connect to the db. Bailing early."
+    exit 1
+  fi
+
+  echo -n "booting ${db}"
+  eval "$launchDB"
+  for _ in $(seq 1 60); do
+    if eval "${testConnection}" &> /dev/null; then
+      echo "connection established to ${db}"
+      return 0
+    fi
+    echo -n "."
+    sleep 1
+  done
+  eval "${testConnection}" || true
+  echo "unable to connect to ${db}"
+  exit 1
+}
+export -f configure_db
