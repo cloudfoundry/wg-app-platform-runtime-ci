71b285b5b24e65bab86f6b0b207f88f23e4efe3b
Maria Shaldybin <mariash@vmware.com>
2023-09-27 18:55:04 +0000

    Update bosh-vendor-package task
    Use package name from the directory instead of parameter
    to dynamically load in pipeline

--- shared/tasks/bosh-vendor-package/task.bash
+++ shared/tasks/bosh-vendor-package/task.bash
@@ -6,6 +6,10 @@ set -o pipefail
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TASK_NAME="$(basename "$THIS_FILE_DIR")"
 export TASK_NAME
+PACKAGE_NAME="$(cat package-name/name)"
+export PACKAGE_NAME
+OLD_PACKAGE_NAME="$(cat package-name/old-name)"
+export OLD_PACKAGE_NAME
 source "$THIS_FILE_DIR/../../../shared/helpers/helpers.bash"
 source "$THIS_FILE_DIR/../../../shared/helpers/git-helpers.bash"
 source "$THIS_FILE_DIR/../../../shared/helpers/bosh-helpers.bash"
@@ -27,6 +31,10 @@ function run() {
     bosh vendor-package "${PACKAGE_NAME}" ../package-release
   fi
 
+  if [[ -n "${OLD_PACKAGE_NAME}" ]] && [[ "${OLD_PACKAGE_NAME}" != "${PACKAGE_NAME}" ]]; then
+    rm -rf packages/"${OLD_PACKAGE_NAME}"
+    sed -i "s/${OLD_PACKAGE_NAME}/${PACKAGE_NAME}/g" packages/**/spec jobs/**/spec
+  fi
 
   if [[ -n $(git status --porcelain) ]]; then
     echo "changes detected, will commit..."
