2112456037c9833ca67f4478e196c2da5c7b37e7
Maria Shaldybin <mariash@vmware.com>
2023-09-27 18:34:46 +0000

    Support prefix in golang package name

--- shared/tasks/build-golang-package-name/task.bash
+++ shared/tasks/build-golang-package-name/task.bash
@@ -8,12 +8,15 @@ set -o pipefail
 go_version_file=$PWD/ci/go_version.json
 
 go_minor_version=$(cat ${go_version_file} | jq -r "if (.releases.\"${RELEASE}\" == null) then .default else .releases.\"${RELEASE}\" end")
+package_name=$(if [[ -n $PREFIX ]]; then echo "$PREFIX-golang-${go_minor_version}-${PLATFORM}"; else echo "golang-${go_minor_version}-${PLATFORM}"; fi)
 
 cat <<HERE > $PWD/package-name/name
-golang-${go_minor_version}-${PLATFORM}
+${package_name}
 HERE
 
-old_package_name=$(find repo/packages -name "golang-*-${PLATFORM}" -printf '%f')
+old_package_glob=$(if [[ -n $PREFIX ]]; then echo "$PREFIX-golang-*-${PLATFORM}"; else echo "golang-*-${PLATFORM}"; fi)
+
+old_package_name=$(find repo/packages -name "${old_package_glob}" -printf '%f')
 cat <<HERE > $PWD/package-name/old-name
 ${old_package_name}
 HERE
