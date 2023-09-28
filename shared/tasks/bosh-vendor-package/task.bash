a5f7eeb52744d3d9122d41233193e1f151176ba8
Amin Jamali <ajamali@vmware.com>
2023-09-28 13:23:20 +0000

    Alternate implementation for build-golang-package-name
    - build-golang-package-name's output is only used when doing the sed
      command to the release. Instead of creating new conventions for
      output, we are modifying the release the way it needs to be, so that
      it can get passed down to the next task in the form "bumped-repo".
    
    - Rename outputs to be in the same format as "bump-dependencies-go-mod"
      to be "bumped-repo". The intention here is that a generic name like
      "package-name" doesn't indicate that there was an action take for that
      to be generated. Our task they all should have verb, and the output
      (if applicable), should be in the format of it's past tense, so that
      if anyone find themselves looking at the pipeline, know by convention
      that a "bumped-*" resource, is generated from a "bump-*" action
      previously.
    
    - This will keep bosh-vendor-package as intended without a custom logic
      to get name from another input. Additionally, PACKAGE_NAME will now
      accept a glob so that the name of the package doesn't have to harcoded
      and it could become "golang-*-linux".

--- shared/tasks/bosh-vendor-package/task.bash
+++ shared/tasks/bosh-vendor-package/task.bash
@@ -6,12 +6,6 @@ set -o pipefail
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TASK_NAME="$(basename "$THIS_FILE_DIR")"
 export TASK_NAME
-if [[ -z ${PACKAGE_NAME} ]]; then
-  PACKAGE_NAME="$(cat package-name/name)"
-  export PACKAGE_NAME
-fi
-OLD_PACKAGE_NAME="$(cat package-name/old-name)"
-export OLD_PACKAGE_NAME
 source "$THIS_FILE_DIR/../../../shared/helpers/helpers.bash"
 source "$THIS_FILE_DIR/../../../shared/helpers/git-helpers.bash"
 source "$THIS_FILE_DIR/../../../shared/helpers/bosh-helpers.bash"
@@ -25,27 +19,25 @@ function run() {
   local private_yml="./config/private.yml"
   bosh_configure_private_yml $private_yml
 
-  debug "bosh vendor for package: ${PACKAGE_NAME} and prefix: ${PACKAGE_PREFIX}"
+  local package_name=$(basename ./packages/${PACKAGE_NAME})
+  debug "Expanding glob from:${PACKAGE_NAME} to:${package_name}"
+
+  debug "bosh vendor for package: ${package_name} and prefix: ${PACKAGE_PREFIX}"
 
   if [[ -n "${PACKAGE_PREFIX}" ]]; then
-    bosh vendor-package "${PACKAGE_NAME}" ../package-release --prefix "${PACKAGE_PREFIX}"
+    bosh vendor-package "${package_name}" ../package-release --prefix "${PACKAGE_PREFIX}"
   else
-    bosh vendor-package "${PACKAGE_NAME}" ../package-release
-  fi
-
-  if [[ -n "${OLD_PACKAGE_NAME}" ]] && [[ "${OLD_PACKAGE_NAME}" != "${PACKAGE_NAME}" ]]; then
-    rm -rf packages/"${OLD_PACKAGE_NAME}"
-    sed -i "s/${OLD_PACKAGE_NAME}/${PACKAGE_NAME}/g" packages/**/spec jobs/**/spec
+    bosh vendor-package "${package_name}" ../package-release
   fi
 
   if [[ -n $(git status --porcelain) ]]; then
     echo "changes detected, will commit..."
     git add --all
-    message="Upgrade ${PACKAGE_NAME}"
+    message="Upgrade ${package_name}"
 
     if [[ -x ../package-release/scripts/get-package-version.sh ]]; then
-      fingerprint="$(yq .fingerprint < "packages/${PACKAGE_NAME}/spec.lock")"
-      pkg_version=$(cd ../package-release && ./scripts/get-package-version.sh "${fingerprint}" "${PACKAGE_NAME}")
+      fingerprint="$(yq .fingerprint < "packages/${package_name}/spec.lock")"
+      pkg_version=$(cd ../package-release && ./scripts/get-package-version.sh "${fingerprint}" "${package_name}")
       message="${message} (${pkg_version})"
     fi
     git commit -m "${message}"
---
+++ shared/tasks/bump-golang-package-name/task.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -eEu
+set -o pipefail
+
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TASK_NAME="$(basename "$THIS_FILE_DIR")"
+export TASK_NAME
+source "$THIS_FILE_DIR/../../../shared/helpers/helpers.bash"
+source "$THIS_FILE_DIR/../../../shared/helpers/bosh-helpers.bash"
+unset THIS_FILE_DIR
+
+function run() {
+    local go_version_file="$PWD/$GO_VERSION_FILE"
+
+    pushd repo > /dev/null
+
+    if ! [[ -f "$go_version_file" ]]; then
+        echo "Missing $GO_VERSION_FILE file"
+        exit 1
+    fi
+
+    if [[ -z "${PLATFORM}" ]]; then
+        echo "Missing PLATFORM enviornment variable"
+        exit 1
+    fi
+
+    local release_name=$(bosh_release_name)
+    local go_minor_version=$(cat ${go_version_file} | jq -r "if (.releases.\"${release_name}\" == null) then .default else .releases.\"${release_name}\" end")
+    local from_package to_package
+    if [[ -n "${PREFIX}" ]]; then
+        from_package=$(basename ./packages/$PREFIX-golang-*-${PLATFORM})
+        to_package="$PREFIX-golang-${go_minor_version}-${PLATFORM}"
+    else
+        from_package=$(basename ./packages/golang-*-${PLATFORM})
+        to_package="golang-${go_minor_version}-${PLATFORM}"
+    fi
+    echo "Replacing bosh package from:${from_package} to:${to_package}"
+    sed -i "s/${from_package}/${to_package}/g" packages/**/spec jobs/**/spec
+    rm -rf "packages/${from_package}"
+
+    rsync -a $PWD/ "../bumped-repo"
+    popd > /dev/null
+
+}
+
+trap 'err_reporter $LINENO' ERR
+run "$@"
