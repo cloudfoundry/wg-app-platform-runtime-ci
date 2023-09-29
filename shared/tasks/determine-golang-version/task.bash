85fdba8d8e43df3fc490b617bae2a59f258fcb3a
Renee Chu <reneec@vmware.com>
2023-09-29 19:46:45 +0000

    task fixes

--- shared/tasks/determine-golang-version/task.bash
+++ shared/tasks/determine-golang-version/task.bash
@@ -3,10 +3,13 @@
 set -eEu
 set -o pipefail
 
+TASK_NAME="$(basename "$THIS_FILE_DIR")"
+export TASK_NAME
+source "$THIS_FILE_DIR/../../../shared/helpers/helpers.bash"
+unset THIS_FILE_DIR
+
 function run() {
     local go_version_file="$PWD/$GO_VERSION_FILE"
-
-    pushd repo > /dev/null
 
     if ! [[ -f "$go_version_file" ]]; then
         echo "Missing $GO_VERSION_FILE file"
