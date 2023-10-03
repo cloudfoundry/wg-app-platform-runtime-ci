836340aaa256120f277702367f45bb60e064ae74
Maria Shaldybin <mariash@vmware.com>
2023-10-03 19:18:57 +0000

    Retry for 10 minutes to pull image

--- shared/tasks/determine-image-version/task.bash
+++ shared/tasks/determine-image-version/task.bash
@@ -2,6 +2,8 @@
 
 set -eEu
 set -o pipefail
+export RETRY_INTERVAL=10
+export MAX_RETRIES=60
 
 function run() {
     local go_version_file="$PWD/$GO_VERSION_FILE"
@@ -16,7 +18,19 @@ function run() {
     local release_name=$(bosh_release_name)
     local go_minor_version=$(cat ${go_version_file} | jq -r "if (.releases.\"${release_name}\" == null) then .default else .releases.\"${release_name}\" end")
 
-    digest=$(curl -s -H "Accept: application/json"  https://hub.docker.com/v2/repositories/${IMAGE}/tags | jq "[.results[] | select(.name | startswith(\"go-${go_minor_version}\")) ] | sort_by(.name) | reverse[0] | .digest")
+    for (( i = 0; i <= MAX_RETRIES; i++ ))
+    do
+        set +e
+        digest=$(curl -s -H "Accept: application/json"  https://hub.docker.com/v2/repositories/${IMAGE}/tags | jq "[.results[] | select(.name | startswith(\"go-${go_minor_version}\")) ] | sort_by(.name) | reverse[0] | .digest")
+        set -e
+
+        if [ -n "$digest" ]; then
+           break
+        fi
+
+        echo -n "."
+        sleep $RETRY_INTERVAL
+    done
 
     popd > /dev/null
 
