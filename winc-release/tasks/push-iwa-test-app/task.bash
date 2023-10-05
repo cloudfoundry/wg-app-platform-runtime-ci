5a66a7454e49dc40cd420228d428d7d0ee91ef01
Brandon Roberson <broberson@vmware.com>
2023-10-05 15:18:57 +0000

    WIP - add push-iwa-test-app task
    Signed-off-by: Amelia Downs <adowns@vmware.com>

---
+++ winc-release/tasks/push-iwa-test-app/task.bash
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -eEu
+set -o pipefail
+
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export TASK_NAME="$(basename $THIS_FILE_DIR)"
+source "$THIS_FILE_DIR/../../../shared/helpers/helpers.bash"
+source "$THIS_FILE_DIR/../../../shared/helpers/cf-helpers.bash"
+
+unset THIS_FILE_DIR
+
+ORG_NAME="iwa-org"
+SPACE_NAME="iwa-space"
+function run(){
+    local task_tmp_dir="${1:?provide temp dir for task}"
+    shift 1
+
+    cf_target
+    cf_command "create-org ${ORG_NAME}"
+    cf_command "create-space -o ${ORG_NAME} ${SPACE_NAME}"
+    cf_command "target -o ${ORG_NAME} -s ${SPACE_NAME}"
+
+cat << EOF > "${task_tmp_dir}/manifest.yml" 
+applications:
+- name: iwa-test-app
+  memory: 1G
+  stack: windows
+  buildpacks:
+    - hwc_buildpack
+  routes:
+    - route: ${CF_TCP_DOMAIN}:1030
+      protocol: tcp
+EOF
+
+    pushd repo/src/WindowsAuth > /dev/null
+    cf_command "push --manifest ${task_tmp_dir}/manifest.yml" 
+}
+
+function cleanup() {
+    rm -rf $task_tmp_dir
+}
+
+task_tmp_dir="$(mktemp -d -t 'XXXX-task-tmp-dir')"
+trap cleanup EXIT
+trap 'err_reporter $LINENO' ERR
+run $task_tmp_dir "$@"
---
+++ winc-release/tasks/verify-iwa-login/task.ps1
@@ -0,0 +1,11 @@
+$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+. "$PSScriptRoot\..\..\..\shared\helpers\helpers.ps1"
+
+function Run
+{
+    <REPLACE_ME>
+}
+
+Run
