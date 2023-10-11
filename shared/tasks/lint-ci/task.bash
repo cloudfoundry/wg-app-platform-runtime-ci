a19670a788006801a98f422d2dfda9d8b1c0dca0
Maria Shaldybin <mariash@vmware.com>
2023-10-11 17:55:07 +0000

    Move go_version_example

--- shared/tasks/lint-ci/task.bash
+++ shared/tasks/lint-ci/task.bash
@@ -195,7 +195,7 @@ function allowed_task_files() {
 
 function allowed_dirs() {
     debug "Running allowed_dirs function"
-    local release_list="garden-runc-release|routing-release|winc-release|nats-release|healthchecker-release|cf-networking-release|silk-release"
+    local release_list="garden-runc-release|routing-release|winc-release|nats-release|healthchecker-release|cf-networking-release|silk-release|envoy-nginx-release"
     local dir_patterns
     dir_patterns="$(cat <<EOF
 ^./bin/*$
