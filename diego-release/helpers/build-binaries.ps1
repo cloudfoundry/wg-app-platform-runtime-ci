b32c96ac5e87c9667ba61ae7737ef7ddb1e2f26a
Amin Jamali <ajamali@vmware.com>
2023-10-17 18:54:44 +0000

    Add build-binaries.ps1 for diego-release's windows
    Signed-off-by: Marc Paquette <mpaquette@vmware.com>

---
+++ diego-release/helpers/build-binaries.ps1
@@ -0,0 +1,32 @@
+. "$PSScriptRoot\..\..\shared\helpers\helpers.ps1"
+
+function Build-Nats-Server
+{
+    Param
+        (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $Source,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $Target
+        )
+
+        Verify-Go
+
+        $env:TEMP="/var/vcap/data/tmp"
+        $env:TMP="/var/vcap/data/tmp"
+        $BuiltDir=$(Split-Path $Target -Leaf)
+        $Target = Update-Dir-If-Symlink "$Target"
+        $Target = Join-Path "$Target" "nats-server"
+        New-Item -ItemType Directory -Force -Path "$Target"
+
+        Push-Location "$Source"
+        go.exe build -o "$Target\run.exe" .
+        if ($LastExitCode -ne 0) {
+            exit 1
+        }
+
+    Pop-Location
+
+    $PS1FILE='$env:NATS_SERVER_BINARY="$PWD/{0}"' -f "$BuiltDir\nats-server\run.exe", 
+        Set-Content -Path "$Target/run.ps1" -Value $PS1FILE -Encoding Asci
+}
