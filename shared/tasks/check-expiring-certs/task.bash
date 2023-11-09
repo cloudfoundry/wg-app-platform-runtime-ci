0e9c3d6d8d95e1a4bab7381d03e13f7424aafc35
Amin Jamali <ajamali@vmware.com>
2023-11-09 13:49:57 +0000

    Fix another path issue
    Signed-off-by: Marc Paquette <mpaquette@vmware.com>

--- shared/tasks/check-expiring-certs/task.bash
+++ shared/tasks/check-expiring-certs/task.bash
@@ -5,7 +5,7 @@ set -o pipefail
 
 echo "Checking for expired certs"
 
-expired_certs=$(find ./repo -name *.crt | xargs -I{} /ci/shared/tasks/check-expiring-certs/check.rb {} 60)
+expired_certs=$(find ./repo -name *.crt | xargs -I{} ./ci/shared/tasks/check-expiring-certs/check.rb {} 60)
 if [ ! -z "$expired_certs" ]; then
   echo "Found certs expiring in 60 days:"
   echo "$expired_certs"
