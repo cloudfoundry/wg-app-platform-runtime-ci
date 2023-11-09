e489c24533e87c0802c475664cf545748388306c
Amin Jamali <ajamali@vmware.com>
2023-11-09 13:35:19 +0000

    Add create-pr task
    Signed-off-by: Marc Paquette <mpaquette@vmware.com>

---
+++ shared/tasks/create-pr/task.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -eEu
+set -o pipefail
+
+function create_pr() {
+    local title="${1:?Provide title}"
+    local description="${1:?Provide title}"
+    gh pr create --title "${title}" --body "${description}" --label "${LABEL}" --base develop --head "${BRANCH}"
+}
+
+function run(){
+    local title=$(git show "origin/$BRANCH" --pretty=format:"%s" --no-patch)
+    local description=$(git show "origin/$BRANCH" --pretty=format:"%b" --no-patch)
+
+    set +e
+    create_pr "${title}" "${description}"
+    PR_EXIT_CODE=$?
+    set -e
+
+    if [ "$PR_EXIT_CODE" -eq 1 ]; then
+        echo "PR could not be created because one already exists."
+
+        gh pr list --head "${BRANCH}" --jq .[0].title --json title | grep "${TITLE}"
+        if [ "$?" -eq 1 ]; then
+            echo "The existing PR is stale. Deleting old PR in favor of the new one."
+
+    #  close old PR
+    gh pr close "${BRANCH}"
+
+    #  resubmit new PR
+    create_pr "${title}" "${description}"
+else
+    echo "The existing PR is identical. No need to create a new PR."
+        fi
+    fi
+}
+
+run "$@"
