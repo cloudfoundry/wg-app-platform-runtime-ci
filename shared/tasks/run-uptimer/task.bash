a8f73c2dcb53b76f18b931b184e8b931fd304811
Amin Jamali <ajamali@vmware.com>
2023-12-07 12:36:49 +0000

    Add bosh_target for run-uptimer

--- shared/tasks/run-uptimer/task.bash
+++ shared/tasks/run-uptimer/task.bash
@@ -3,7 +3,13 @@
 set -eEu
 set -o pipefail
 
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export TASK_NAME="$(basename $THIS_FILE_DIR)"
+source "$THIS_FILE_DIR/../../../shared/helpers/bosh-helpers.bash"
+unset THIS_FILE_DIR
+
 function run() {
+  bosh_target
   local config="$(ls config/uptimer*.json)"
   uptimer -configFile "$config"
 }
