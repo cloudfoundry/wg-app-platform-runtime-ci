41b354a16fa39f096af06f50d381cca00610fb5e
Amin Jamali <amin.jamali@broadcom.com>
2024-01-19 16:43:25 +0000

    fix linters for winc-release

--- winc-release/linters/sync-package-specs.bash
+++ winc-release/linters/sync-package-specs.bash
@@ -15,12 +15,12 @@ function run() {
 
   pushd "${repo_path}" > /dev/null
 
-  GOOS=windows sync_package winc                  -app  code.cloudfoundry.org/winc/cmd/winc
-  GOOS=windows sync_package winc-network-hns-acls -app  code.cloudfoundry.org/winc/cmd/winc-network
-  GOOS=windows sync_package groot                 -app  code.cloudfoundry.org/groot-windows
-  GOOS=windows sync_package diff-exporter         -app  code.cloudfoundry.org/diff-exporter
-  GOOS=windows sync_package cert-injector         -app  code.cloudfoundry.org/cert-injector
-  GOOS=windows sync_package certsplitter-fs       -app  code.cloudfoundry.org/certsplitter/cmd/certsplitter
+  GOOS=windows sync_package winc winc -app  code.cloudfoundry.org/winc/cmd/winc
+  GOOS=windows sync_package winc-network-hns-acls winc -app  code.cloudfoundry.org/winc/cmd/winc-network
+  GOOS=windows sync_package groot groot-windows -app  code.cloudfoundry.org/groot-windows
+  GOOS=windows sync_package diff-exporter diff-exporter -app  code.cloudfoundry.org/diff-exporter
+  GOOS=windows sync_package cert-injector cert-injector -app  code.cloudfoundry.org/cert-injector
+  GOOS=windows sync_package certsplitter-fs certsplitter -app  code.cloudfoundry.org/certsplitter/cmd/certsplitter
 
   wait
 
@@ -35,39 +35,49 @@ function run() {
 
 function sync_package() {
   bosh_pkg=${1}
+  src_dir=${2}
 
+  shift
   shift
 
   (
   set -e
 
-  cd packages/${bosh_pkg}
+  cd "src/code.cloudfoundry.org/${src_dir}"
+
+  spec=../../../packages/${bosh_pkg}/spec
 
   {
-    cat spec | grep -v '# gosub'
+    cat $spec | grep -v '# gosub'
 
     for package in $(gosub list "$@"); do
       repo=$(echo ${2} | cut -f1-3 -d/)
-      if [ -d "../../src/${repo}/Godeps/_workspace/src/${package}" ]; then
-        package="${repo}/Godeps/_workspace/src/${package}"
+      base_pkg="$(echo $package | cut -f2- -d /)"
+      if [ -d "../../../src/code.cloudfoundry.org/${src_dir}/vendor/${package}" ]; then
+        package="${src_dir}/vendor/${package}"
+      else
+        package="${base_pkg}"
       fi
       echo ${package} | sed -e 's/\(.*\)/  - \1\/*.go # gosub/g'
       if ls ../../src/${package}/*.s >/dev/null 2>&1; then
         echo ${package} | sed -e 's/\(.*\)/  - \1\/*.s # gosub/g'
       fi
-      if ls ../../src/${package}/**/*.c >/dev/null 2>&1; then
-        echo ${package} | sed -e 's/\(.*\)/  - \1\/**\/*.c # gosub/g'
+      if ls ../../src/${package}/*.h >/dev/null 2>&1; then
+        echo ${package} | sed -e 's/\(.*\)/  - \1\/*.h # gosub/g'
       fi
-      if ls ../../src/${package}/**/*.h >/dev/null 2>&1; then
-        echo ${package} | sed -e 's/\(.*\)/  - \1\/**\/*.h # gosub/g'
+      if ls ../../src/${package}/*.c >/dev/null 2>&1; then
+        echo ${package} | sed -e 's/\(.*\)/  - \1\/*.c # gosub/g'
       fi
-      if ls ../../src/${package}/**/*.binpb >/dev/null 2>&1; then
-        echo ${package} | sed -e 's/\(.*\)/  - \1\/**\/*.binpb # gosub/g'
+      if ls ../../src/${package}/Makefile >/dev/null 2>&1; then
+        echo ${package} | sed -e 's/\(.*\)/  - \1\/Makefile # gosub/g'
+      fi
+      if ls ../../src/${package}/*.binpb >/dev/null 2>&1; then
+        echo ${package} | sed -e 's/\(.*\)/  - \1\/*.binpb # gosub/g'
       fi
     done
-  } > spec.new
+  } > $spec.new
 
-  mv spec.new spec
+  mv $spec.new $spec
 )
 }
 
