366db4042891b32303fec9a277c705a73682a52a
Amin Jamali <amin.jamali@broadcom.com>
2024-01-22 16:20:15 +0000

    Add windows-tools-release

---
+++ windows-tools-release/bin/set-repo-pipeline.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+REPO="$DIR/.."
+FLY_TEAM=wg-arp-garden
+
+main() {
+  local pipeline_dir="$(realpath $REPO/pipelines)"
+  fly_login
+  fly_pipeline windows-tools-release -f "${pipeline_dir}/windows-tools-release.yml" \
+    -f "$REPO/../shared/helpers/ytt-helpers.star"
+}
+
+main
