7a1011f41faffad1fb3af6d47d9036878df1c2b2
Amin Jamali <amin.jamali@broadcom.com>
2024-01-23 14:32:44 +0000

    Add bump-repo-version-file task
    Signed-off-by: Brandon Roberson <brandon.roberson@broadcom.com>

---
+++ shared/tasks/bump-repo-version-file/task.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eEu
+set -o pipefail
+
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export TASK_NAME="$(basename $THIS_FILE_DIR)"
+source "$THIS_FILE_DIR/../../../shared/helpers/helpers.bash"
+source "$THIS_FILE_DIR/../../../shared/helpers/git-helpers.bash"
+unset THIS_FILE_DIR
+
+function run(){
+    export VERSION=$(cat version/version)
+    pushd repo > /dev/null
+
+    eval "${BUMP_ACTION}"
+
+    if [[ $(git status --porcelain) ]]; then
+        git add -A .
+        git commit -m "Bump Version to ${VERSION}"
+    fi
+    rsync -a $PWD/ "../bumped-repo"
+    popd > /dev/null
+}
+
+trap 'err_reporter $LINENO' ERR
+run "$@"
