ced108f8056895e84f6bb0556b1ebe08a45a1066
Amin Jamali <amin.jamali@broadcom.com>
2024-01-24 18:54:27 +0000

    Add build-rootfs-metadata task

---
+++ windows2016fs/tasks/build-rootfs-metadata/task.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$version=(cat version/version)
+
+docker run `
+  -v "$PWD\built-metadata:c:\built-metadata" `
+  -w c:\built-metadata`
+  --rm `
+  "cloudfoundry/windows2016fs:$version" `
+  "powershell" "-Command" "Get-Hotfix | Select HotFixID,InstalledOn,Description,InstalledBy > kb-metadata"
+if ($LASTEXITCODE -ne 0) {
+  throw "failed to build metadata"
+}
+
+Write-Output "$env:IMAGE_NAME:$version"
+Get-Content "$PWD\built-metadata\kb-metadata"
+
+
