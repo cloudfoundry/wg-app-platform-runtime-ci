cadc7c5c8cdd5701a165bb37b29121d0aa0dd235
Brandon Roberson <brandon.roberson@broadcom.com>
2024-02-05 17:15:05 +0000

    Use cf manifest instead of `bosh releases`

--- shared/tasks/bosh-export-release/task.bash
+++ shared/tasks/bosh-export-release/task.bash
@@ -11,19 +11,30 @@ unset THIS_FILE_DIR
 
 
 function run(){
+    local task_tmp_dir="${1:?provide temp dir for task}"
+    shift 1
+
     bosh_target
+    local cf_manifest="$(mktemp -p ${task_tmp_dir} -t 'XXXXX-cf.yml')"
+    bosh_manifest > "${cf_manifest}"
+
     pushd repo > /dev/null
     local release_name=$(bosh_release_name)
     popd > /dev/null
 
-    local release=$(bosh releases --json | jq -r --arg release "${release_name}" '.Tables[].Rows[] | select(.name == $release and (.version | contains("*"))) | .name + "/" + .version' | tr -d "*" | sort -V | tail -1)
+    local release=$(release="${release_name}" yq '.releases | .[] | select(.name==env(release)) | .version' "${cf_manifest}")
     local stemcell=$(bosh stemcells --json | jq -r --arg os "${OS}" '.Tables[].Rows[] | select(.os | contains($os)) | select(.version | contains("*")) | .os + "/" + .version' | tr -d "*" | sort -V | tail -1)
 
 
     debug "Running 'bosh export-release -d ${DEPLOYMENT_NAME} ${release} ${stemcell}'"
     bosh export-release -d "${DEPLOYMENT_NAME}" "${release}" "${stemcell}"
-
 }
 
+function cleanup() {
+    rm -rf "${task_tmp_dir}"
+}
+
+task_tmp_dir="$(mktemp -d -t 'XXXX-task-tmp-dir')"
+trap cleanup EXIT
 trap 'err_reporter $LINENO' ERR
-run "$@"
+run $task_tmp_dir "$@"
