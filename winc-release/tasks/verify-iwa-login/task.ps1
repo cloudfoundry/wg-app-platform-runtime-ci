b21df1389f8db41370a8d3af27ebdbda187894cc
Geoff Franks <geoff.franks@broadcom.com>
2024-02-07 17:41:36 +0000

    Add cookiejar for verify-iwa-login

--- winc-release/tasks/verify-iwa-login/task.ps1
+++ winc-release/tasks/verify-iwa-login/task.ps1
@@ -2,7 +2,7 @@ $usernameWithDomain = $env:IWA_DOMAIN + '\' + $env:IWA_USERNAME
 Write-Host "Attempting to curl $env:IWA_TEST_APP_ROUTE with $usernameWithDomain"
 
 $authString = $usernameWithDomain + ':' + $env:IWA_PASSWORD
-$curlOutput = curl.exe -s $env:IWA_TEST_APP_ROUTE --negotiate -u $authString | Out-String
+$curlOutput = curl.exe -s $env:IWA_TEST_APP_ROUTE --negotiate -u $authString -c ./cookies | Out-String
 
 $testString =  "Logged in as $env:IWA_DOMAIN\\$env:IWA_USERNAME via method Negotiate."
 if ($curlOutput -like "*$testString*") {
