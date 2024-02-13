a1e20e9a1849f5101e84f5aa675fd7ac35c8b438
Geoff Franks <geoff.franks@broadcom.com>
2024-02-13 20:14:59 +0000

    more setup-iwa-test-app fixes

--- winc-release/tasks/setup-iwa-test-app/task.bash
+++ winc-release/tasks/setup-iwa-test-app/task.bash
@@ -14,7 +14,7 @@ cf_target
 cf_create_tcp_domain
 
 echo '[{"protocol":"all","destination":"'"${AD_SUBNET}"'"}]' > dc.json
-if ! cf_command security-group active-directory-domain-controllers; then
+if ! cf_command security-group active-directory-domain-controllers >& /dev/null; then
   cf_command create-security-group active-directory-domain-controllers dc.json
 else
   cf_command update-security-group active-directory-domain-controllers dc.json
@@ -27,5 +27,5 @@ cf_command create-org "${APP_ORG}"
 cf_command create-space -o "${APP_ORG}" "${APP_SPACE}"
 cf_command target -o "${APP_ORG}" -s "${APP_SPACE}"
 cf_command push "${APP_DOMAIN}" -m 1g -s windows -i 2 -b hwc_buildpack -p repo/src/WindowsAuth --no-route
-cf_command map_route "${APP_DOMAIN}" "${APP_DOMAIN}" -n "${APP_HOSTNAME}"
-cf_command map_route "${APP_DOMAIN}" "${CF_TCP_DOMAIN}" -p "${APP_TCP_PORT}"
+cf_command map-route "${APP_DOMAIN}" "${APP_DOMAIN}" -n "${APP_HOSTNAME}"
+cf_command map-route "${APP_DOMAIN}" "${CF_TCP_DOMAIN}" --port "${APP_TCP_PORT}"
