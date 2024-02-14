5c3978728571d584c46817204ae05bbb9d0926b2
Amin Jamali <amin.jamali@broadcom.com>
2024-02-14 16:57:03 +0000

    fix typo

--- envoy-nginx-release/helpers/bump-bosh-blobs.bash
+++ envoy-nginx-release/helpers/bump-bosh-blobs.bash
@@ -28,7 +28,7 @@ function run() {
         local blob_name="$(basename blobs/${bosh_blob_path})"
         local dir_name="$(dirname ${bosh_blob_path})"
         bosh remove-blob "${dir_name}/${blob_name}"
-        bosh add-blob "${zip_name}" ${dir_name}/${zip_name}" 
+        bosh add-blob "${zip_name}" "${dir_name}/${zip_name}" 
         rm -rf "${zip_name}" nginx.zip nginx.exe
     fi
 }
