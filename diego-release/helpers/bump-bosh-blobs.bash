77f940152a9b8bdb8ad0cb2f6a70a2241f99e23c
Amin Jamali <amin.jamali@broadcom.com>
2024-03-15 15:25:58 +0000

    add bump-bosh-blobs for diego

---
+++ diego-release/helpers/bump-bosh-blobs.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export TASK_NAME="$(basename $THIS_FILE_DIR)"
+source "$THIS_FILE_DIR/../../shared/helpers/helpers.bash"
+source "$THIS_FILE_DIR/../../shared/helpers/git-helpers.bash"
+unset THIS_FILE_DIR
+
+function run() {
+    local repo_path=${1:?Provide a path to the repository}
+    local bosh_blob_path=${2:?Provide a regex path for bosh-blob}
+    local blob=${3:?Provide a path to new blob}
+
+    if [[ "$bosh_blob_path" == 'jq/jq-*-linux-amd64.tgz' ]]; then
+        echo "Bumping jq blob"
+        pushd "${blob}" > /dev/null
+        local version=$(cat version | tr -d 'jq')
+        local tgz_name="jq-${version}-linux-amd64.tgz"
+        mv jq-linux-amd64 jq
+        tar czvf "${tgz_name}" jq
+        popd > /dev/null
+
+        if [[ -f $(find ./blobs  -type f -regextype posix-extended -regex ".*$tgz_name") ]]; then
+            echo "$tgz_name already exists, skippping"
+            return
+        fi
+
+        local blob_name="$(basename blobs/${bosh_blob_path})"
+        local dir_name="$(dirname ${bosh_blob_path})"
+        bosh remove-blob "${dir_name}/${blob_name}"
+        bosh add-blob "${tgz_name}" "${blob}/${zip_name}" 
+    fi
+}
+
+run "$@"
