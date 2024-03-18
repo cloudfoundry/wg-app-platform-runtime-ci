cbce29cc134df4b7b525f4c4267eebcc5412386c
Amin Jamali <amin.jamali@broadcom.com>
2024-03-18 18:34:44 +0000

    add staticcheck

--- windows-tools-release/helpers/bump-bosh-blobs.bash
+++ windows-tools-release/helpers/bump-bosh-blobs.bash
@@ -17,7 +17,7 @@ function run() {
     if [[ "$bosh_blob_path" == 'yq/yq-*-windows-amd64.exe' ]]; then
         echo "Bumping yq blob"
         pushd "${blob}" > /dev/null
-        local version=$(cat version | tr -d 'v')
+        local version=$(cat version)
         local exe_name="yq-${version}-windows-amd64.exe"
         mv yq_windows_amd64.exe "${exe_name}"
         popd > /dev/null
@@ -31,31 +31,12 @@ function run() {
         local dir_name="$(dirname ${bosh_blob_path})"
         bosh remove-blob "${dir_name}/${blob_name}"
         bosh add-blob "${blob}/${exe_name}" "${dir_name}/${exe_name}"
-    elif [[ "$bosh_blob_path" == 'tar/tar-*.tgz' ]]; then
-        echo "Bumping tar blob"
+    elif [[ "$bosh_blob_path" == 'staticcheck/staticcheck-*-windows-amd64.tar.gz' ]]; then
+        echo "Bumping staticcheck blob"
         pushd "${blob}" > /dev/null
         local version=$(cat version)
-        local tgz_name="tar-${version}.tgz"
-        tar czvf "${tgz_name}" tar-*.exe
-        popd > /dev/null
-
-        if [[ -f $(find ./blobs  -type f -regextype posix-extended -regex ".*$tgz_name") ]]; then
-            echo "$tgz_name already exists, skippping"
-            return
-        fi
-
-        local blob_name="$(basename blobs/${bosh_blob_path})"
-        local dir_name="$(dirname ${bosh_blob_path})"
-        bosh remove-blob "${dir_name}/${blob_name}"
-        bosh add-blob "${blob}/${tgz_name}" "${dir_name}/${tgz_name}"
-    elif [[ "$bosh_blob_path" == 'winpty/winpty-*.tgz' ]]; then
-        echo "Bumping winpty blob"
-        pushd "${blob}" > /dev/null
-        local version=$(cat version | tr -d 'winpty ')
-        local tgz_name="winpty-${version}.tgz"
-        unzip winpty-*.zip
-        mv ./x64/bin/winpty* .
-        tar czvf "${tgz_name}" LICENSE winpty.dll winpty-agent.exe
+        local tgz_name="staticcheck-${version}-windows-amd64.tar.gz"
+        mv staticcheck_windows_amd64.tar.gz  "${tgz_name}"
         popd > /dev/null
 
         if [[ -f $(find ./blobs  -type f -regextype posix-extended -regex ".*$tgz_name") ]]; then
