6d53d4a763979f0f9d5eaa5b67aebaadf7f857a0
Amin Jamali <amin.jamali@broadcom.com>
2024-03-21 15:16:07 +0000

    add automake

--- garden-runc-release/helpers/bump-bosh-blobs.bash
+++ garden-runc-release/helpers/bump-bosh-blobs.bash
@@ -31,14 +31,29 @@ function run() {
         local dir_name="$(dirname ${bosh_blob_path})"
         bosh remove-blob "${dir_name}/${blob_name}"
         bosh add-blob "${blob}/${tgz_name}" "${dir_name}/${tgz_name}"
-    fi
-
-    if [[ "$bosh_blob_path" == 'autoconf/autoconf-*.tar.gz' ]]; then
+    elif [[ "$bosh_blob_path" == 'autoconf/autoconf-*.tar.gz' ]]; then
         echo "Bumping autoconf blob"
         pushd "${blob}" > /dev/null
         local version=$(git describe --tags --abbrev=0 | tr -d '[a-z]')
         local tgz_name="autoconf-${version}.tar.gz"
         wget "https://ftp.gnu.org/gnu/autoconf/autoconf-${version}.tar.gz" -o "${tgz_name}"
+        popd > /dev/null
+
+        if [[ -f $(find ./blobs  -type f -regextype posix-extended -regex ".*$tgz_name") ]]; then
+            echo "$tgz_name already exists, skippping"
+            return
+        fi
+
+        local blob_name="$(basename blobs/${bosh_blob_path})"
+        local dir_name="$(dirname ${bosh_blob_path})"
+        bosh remove-blob "${dir_name}/${blob_name}"
+        bosh add-blob "${blob}/${tgz_name}" "${dir_name}/${tgz_name}"
+    elif [[ "$bosh_blob_path" == 'automake/automake-*.tar.gz' ]]; then
+        echo "Bumping automake blob"
+        pushd "${blob}" > /dev/null
+        local version=$(git describe --tags --abbrev=0 | tr -d '[a-z]')
+        local tgz_name="automake{version}.tar.gz"
+        wget "https://ftp.gnu.org/gnu/automake/automake-${version}.tar.gz" -o "${tgz_name}"
         popd > /dev/null
 
         if [[ -f $(find ./blobs  -type f -regextype posix-extended -regex ".*$tgz_name") ]]; then
