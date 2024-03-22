caa82c0a5b2ab938e1a691399eeb116b9316bccb
Amin Jamali <amin.jamali@broadcom.com>
2024-03-22 19:24:42 +0000

    Add libseccomp libtool

--- garden-runc-release/helpers/bump-bosh-blobs.bash
+++ garden-runc-release/helpers/bump-bosh-blobs.bash
@@ -151,6 +151,23 @@ function run() {
         local dir_name="$(dirname ${bosh_blob_path})"
         bosh remove-blob "${dir_name}/${blob_name}"
         bosh add-blob "${blob}/${tgz_name}" "${dir_name}/${tgz_name}"
+    elif [[ "$bosh_blob_path" == 'libtool/libtool-*.tar.gz' ]]; then
+        echo "Bumping libtool blob"
+        pushd "${blob}" > /dev/null
+        local version=$(git describe --tags --abbrev=0 | tr -d '[a-z]')
+        local tgz_name="libtool-${version}.tar.gz"
+        wget  -o "${tgz_name}" "https://ftpmirror.gnu.org/libtool/libtool-${version}.tar.gz"
+        popd > /dev/null
+
+        if [[ -f $(find ./blobs  -type f -regextype posix-extended -regex ".*$tgz_name") ]]; then
+            echo "$tgz_name already exists, skippping"
+            return
+        fi
+
+        local blob_name="$(basename blobs/${bosh_blob_path})"
+        local dir_name="$(dirname ${bosh_blob_path})"
+        bosh remove-blob "${dir_name}/${blob_name}"
+        bosh add-blob "${blob}/${tgz_name}" "${dir_name}/${tgz_name}"
     fi
 }
 
