85abd38e07722a39a14a58ec3e954d90b5894d01
Amin Jamali <amin.jamali@broadcom.com>
2024-03-26 16:34:12 +0000

    Add zlib

--- garden-runc-release/helpers/bump-bosh-blobs.bash
+++ garden-runc-release/helpers/bump-bosh-blobs.bash
@@ -306,6 +306,22 @@ function run() {
         local dir_name="$(dirname ${bosh_blob_path})"
         bosh remove-blob "${dir_name}/${blob_name}"
         bosh add-blob "${blob}/${tgz_name}" "${dir_name}/${tgz_name}"
+    elif [[ "$bosh_blob_path" == 'zlib/zlib-*.tar.gz' ]]; then
+        echo "Bumping zlib blob"
+        pushd "${blob}" > /dev/null
+        local version=$(cat version)
+        local tgz_name="zlib-${version}.tar.gz"
+        popd > /dev/null
+
+        if [[ -f $(find ./blobs  -type f -regextype posix-extended -regex ".*$tgz_name") ]]; then
+            echo "$tgz_name already exists, skippping"
+            return
+        fi
+
+        local blob_name="$(basename blobs/${bosh_blob_path})"
+        local dir_name="$(dirname ${bosh_blob_path})"
+        bosh remove-blob "${dir_name}/${blob_name}"
+        bosh add-blob "${blob}/${tgz_name}" "${dir_name}/${tgz_name}"
     else
         echo "can't find ${bosh_blob_path}"
         exit 1
