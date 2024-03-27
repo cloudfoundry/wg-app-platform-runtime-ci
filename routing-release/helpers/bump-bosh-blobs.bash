9e68e7a079505ab8f6f499ae4ab5a7de35718141
Marc Paquette <marc.paquette@broadcom.com>
2024-03-27 17:31:59 +0000

    Autobump the haproxy blob

--- routing-release/helpers/bump-bosh-blobs.bash
+++ routing-release/helpers/bump-bosh-blobs.bash
@@ -32,6 +32,25 @@ function run() {
         local dir_name="$(dirname ${bosh_blob_path})"
         bosh remove-blob "${dir_name}/${blob_name}"
         bosh add-blob "${blob}/${tgz_name}" "${dir_name}/${tgz_name}"
+    if [[ "$bosh_blob_path" == 'haproxy/haproxy-*.tar.gz' ]]; then
+        echo "Bumping haproxy blob"
+        pushd "${blob}" > /dev/null
+        local version=$(git describe --tags --abbrev=0 | tr -d '[a-z]-')
+        local tgz_name="haproxy-${version}.tar.gz"
+        local major_minor_version="$(echo ${version} | cut -d'.' -f1,2)"
+        wget -O "${tgz_name}" "https://www.haproxy.org/download/${major_minor_version}/src/haproxy-${version}.tar.gz"
+
+        popd > /dev/null
+
+        if [[ -f $(find ./blobs  -type f -regextype posix-extended -regex ".*$tgz_name") ]]; then
+            echo "$tgz_name already exists, skippping"
+            return
+        fi
+
+        local blob_name="$(basename blobs/${bosh_blob_path})"
+        local dir_name="$(dirname ${bosh_blob_path})"
+        bosh remove-blob "${dir_name}/${blob_name}"
+        bosh add-blob "${blob}/${tgz_name}" "${dir_name}/${tgz_name}"
     else
         echo "can't find ${bosh_blob_path}"
         exit 1
