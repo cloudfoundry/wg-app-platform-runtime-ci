286369140fdfe0f434e4f180aefdd467f8122923
Geoff Franks <geoff.franks@broadcom.com>
2024-06-14 16:08:31 +0000

    Add linters for mapfs-release

---
+++ mapfs-release/linters/sync-package-specs.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "This repo does not make use of per-file package specs. No need to sync them"
+exit 0
---
+++ mapfs-release/linters/sync-submodule-config.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export TASK_NAME="$(basename $THIS_FILE_DIR)"
+source "$THIS_FILE_DIR/../../shared/helpers/helpers.bash"
+source "$THIS_FILE_DIR/../../shared/helpers/git-helpers.bash"
+unset THIS_FILE_DIR
+
+function run() {
+  local repo_path=${1:?Provide a path to the repository}
+  local exit_on_error=${2:-"false"}
+  pushd "${repo_path}" > /dev/null
+
+  rm -rf /tmp/packages
+  cat > /tmp/packages <<EOF
+mapfs
+EOF
+
+cat /tmp/packages | xargs -s 1048576 gosub sync --force-https=true
+
+if [[ "$exit_on_error" == "true" ]]; then
+  git_error_when_diff
+fi
+
+popd > /dev/null
+
+}
+
+verify_binary gosub
+run "$@"
