2e2dde78f7a7897042e7b7bade6fe302480f4919
Amin Jamali <amin.jamali@broadcom.com>
2024-06-21 20:07:34 +0000

    Add missing space

--- shared/tasks/bosh-update-config/task.bash
+++ shared/tasks/bosh-update-config/task.bash
@@ -34,7 +34,8 @@ function run(){
         arguments="${arguments} -o ${op}"
     done
 
-    if [[ $(bosh configs --name=${NAME} --type=${TYPE} --json | jq '.Tables[0].Rows | length') -ne 0]] then
+    if [[ $(bosh configs --name=${NAME} --type=${TYPE} --json | jq '.Tables[0].Rows | length') -ne 0 ]]
+    then
         bosh delete-config -n --name=${NAME} --type=${TYPE}
     fi
 
