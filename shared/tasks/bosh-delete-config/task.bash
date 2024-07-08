84b0350672da6f076ce5b5fdd409696a266c0505
Amin Jamali <amin.jamali@broadcom.com>
2024-07-08 14:35:08 +0000

    Add bosh-delete-config task

---
+++ shared/tasks/bosh-delete-config/task.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eEu
+set -o pipefail
+
+
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export TASK_NAME="$(basename $THIS_FILE_DIR)"
+source "$THIS_FILE_DIR/../../../shared/helpers/helpers.bash"
+source "$THIS_FILE_DIR/../../../shared/helpers/bosh-helpers.bash"
+unset THIS_FILE_DIR
+
+function run(){
+    local task_tmp_dir="${1:?provide temp dir for task}"
+    shift 1
+
+    bosh_target
+
+    if [[ $(bosh configs --name=${NAME} --type=${TYPE} --json | jq '.Tables[0].Rows | length') -ne 0 ]]
+    then
+        bosh delete-config -n --name=${NAME} --type=${TYPE}
+    fi
+
+}
+
+function cleanup() {
+    rm -rf $task_tmp_dir
+}
+
+task_tmp_dir="$(mktemp -d -t 'XXXX-task-tmp-dir')"
+trap cleanup EXIT
+trap 'err_reporter $LINENO' ERR
+run $task_tmp_dir "$@"
