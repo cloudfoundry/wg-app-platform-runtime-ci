88aa14cd8ca7fdaacb298d650697b92c81de9838
Amin Jamali <amin.jamali@broadcom.com>
2024-08-20 15:06:11 +0000

    add nfs-volume-release pipeline
    Signed-off-by: Amelia Downs <amelia.downs@broadcom.com>

---
+++ nfs-volume-release/01-nfs-volume-release.md
@@ -0,0 +1,7 @@
+# Contributing
+
+See the [Contributing.md](./.github/CONTRIBUTING.md) for more information on how to contribute.
+
+# Working Group Charter
+
+This repository is maintained by [Service Management](https://github.com/cloudfoundry/community/blob/main/toc/working-groups/service-management.md) under `Volume Services` area.
---
+++ nfs-volume-release/bin/set-repo-pipeline.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+REPO="$DIR/.."
+FLY_TEAM=wg-sm-volume-services
+
+main() {
+  local pipeline_dir="$(realpath $REPO/pipelines)"
+  fly_login
+  fly_pipeline nfs-volume-release -f "${pipeline_dir}/nfs-volume-release.yml" \
+    -f "$REPO/index.yml" \
+    -f "$REPO/../shared/helpers/ytt-helpers.star"
+}
+
+main
