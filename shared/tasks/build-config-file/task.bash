fc271364872725adb829cd6fe0fd05a27484ebc5
Amin Jamali <amin.jamali@broadcom.com>
2024-08-26 11:47:40 +0000

    Add build-config-file task
    this will help build a generic config to be used in the following task

---
+++ shared/tasks/build-config-file/task.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eEu
+set -o pipefail
+
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export TASK_NAME="$(basename $THIS_FILE_DIR)"
+source "$THIS_FILE_DIR/../../../shared/helpers/helpers.bash"
+source "$THIS_FILE_DIR/../../../shared/helpers/bosh-helpers.bash"
+source "$THIS_FILE_DIR/../../../shared/helpers/cf-helpers.bash"
+unset THIS_FILE_DIR
+
+function run(){
+    local task_tmp_dir="${1:?provide temp dir for task}"
+    shift 1
+
+    echo ${CONFIG} | yq -r . > "./built-config-file/config.yml"
+    echo ${CONFIG} | yq -r . -o "json" | jq -r . > "./built-config-file/config.json"
+}
+
+function cleanup() {
+    rm -rf $task_tmp_dir
+}
+
+task_tmp_dir="$(mktemp -d -t 'XXXX-task-tmp-dir')"
+trap cleanup EXIT
+trap 'err_reporter $LINENO' ERR
+run $task_tmp_dir "$@"
