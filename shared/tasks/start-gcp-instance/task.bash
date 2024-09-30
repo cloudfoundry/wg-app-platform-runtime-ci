7ec80d2d075ae7c53b998e21c3d91a760c3071f4
Geoff Franks <geoff.franks@broadcom.com>
2024-09-30 20:46:32 +0000

    Fix bad env var

--- shared/tasks/start-gcp-instance/task.bash
+++ shared/tasks/start-gcp-instance/task.bash
@@ -7,7 +7,7 @@ THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 export TASK_NAME="$(basename $THIS_FILE_DIR)"
 source "$THIS_FILE_DIR/../../../shared/helpers/helpers.bash"
 
-echo "${SERVICE_ACCOUNT_JSON}" /tmp/config.json
+echo "${SERVICE_ACCOUNT_KEY}" /tmp/config.json
 
 gcloud auth activate-service-account --key-file /tmp/config.json
 gcloud compute instances start "${INSTANCE_NAME}" --zone "${ZONE}"
