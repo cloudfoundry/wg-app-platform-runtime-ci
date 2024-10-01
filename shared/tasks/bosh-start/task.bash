96b766a8401078e07cc26264dd4874c39c453e81
Geoff Franks <geoff.franks@broadcom.com>
2024-10-01 16:44:10 +0000

    Set up ssh private key for bosh-start/stop

--- shared/tasks/bosh-start/task.bash
+++ shared/tasks/bosh-start/task.bash
@@ -15,6 +15,9 @@ function run(){
     shift 1
 
     eval "${BOSH_CREDS}"
+    mkdir -p "$(dirname "${JUMPBOX_PRIVATE_KEY}")"
+    echo "${SSH_PRIVATE_KEY}" > "${JUMPBOX_PRIVATE_KEY}"
+    chmod 600 "${JUMPBOX_PRIVATE_KEY}"
     bosh -n -d "${DEPLOYMENT_NAME}" start "${INSTANCE_GROUP}"
 }
 
