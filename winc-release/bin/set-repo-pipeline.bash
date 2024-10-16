d527061157021add3a62a0b8404078440c908522
Brandon Roberson <brandon.roberson@broadcom.com>
2024-10-16 20:44:31 +0000

    Add winc-shepherd test pipeline; Don't update regular pipeline in bin/set-repo-pipeline

--- winc-release/bin/set-repo-pipeline.bash
+++ winc-release/bin/set-repo-pipeline.bash
@@ -9,7 +9,8 @@ FLY_TEAM=wg-arp-garden
 main() {
   local pipeline_dir="$(realpath $REPO/pipelines)"
   fly_login
-  fly_pipeline winc-release -f "${pipeline_dir}/winc-release.yml" \
+  # fly_pipeline winc-release -f "${pipeline_dir}/winc-release.yml" \
+  fly_pipeline winc-shepherd -f "${pipeline_dir}/winc-shepherd.yml" \
     -f "$REPO/index.yml" \
     -f "$REPO/../shared/helpers/ytt-helpers.star"
 }
