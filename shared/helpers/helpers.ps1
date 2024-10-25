182163211c98062bd058cafe14bba883aeedeb34
Geoff Franks <geoff.franks@broadcom.com>
2024-10-25 16:13:14 +0000

    Fix typo

--- shared/helpers/helpers.ps1
+++ shared/helpers/helpers.ps1
@@ -1,4 +1,4 @@
-$env:Path = 'C:\var\vcap\packages\git\bin;' + $env:Path:q
+$env:Path = 'C:\var\vcap\packages\git\bin;' + $env:Path
 
 # work around https://github.com/golang/go/issues/27515
 function Update-Dir-If-Symlink {
