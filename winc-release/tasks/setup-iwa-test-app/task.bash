1ba4e784f4ecf7dfa7f5611c2189d5ce16983046
Brandon Roberson <brandon.roberson@broadcom.com>
2024-12-19 22:28:26 +0000

    Use CF_SYSTEM_DOMAIN instead of APP_DOMAIN because we're using shepherd envs now

--- winc-release/tasks/setup-iwa-test-app/task.bash
+++ winc-release/tasks/setup-iwa-test-app/task.bash
@@ -26,6 +26,6 @@ cf_command bind-running-security-group active-directory-domain-controllers
 cf_command create-org "${APP_ORG}"
 cf_command create-space -o "${APP_ORG}" "${APP_SPACE}"
 cf_command target -o "${APP_ORG}" -s "${APP_SPACE}"
-cf_command push "${APP_DOMAIN}" -m 1g -s windows -i 2 -b hwc_buildpack -p repo/src/WindowsAuth --no-route
+cf_command push "${CF_SYSTEM_DOMAIN}" -m 1g -s windows -i 2 -b hwc_buildpack -p repo/src/WindowsAuth --no-route
 cf_command map-route "${APP_DOMAIN}" "${APP_DOMAIN}" -n "${APP_HOSTNAME}"
 cf_command map-route "${APP_DOMAIN}" "${CF_TCP_DOMAIN}" --port "${APP_TCP_PORT}"
