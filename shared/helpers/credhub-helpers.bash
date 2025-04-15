f80d023188677da5b7df99bf3a06e15c61ed7ec9
Geoff Franks <geoff.franks@broadcom.com>
2025-04-15 20:32:32 +0000

    Fix credhub path for BBL envs

--- shared/helpers/credhub-helpers.bash
+++ shared/helpers/credhub-helpers.bash
@@ -1,21 +1,23 @@
 function credhub_save_lb_cert() {
-    CF_ENVIRONMENT_NAME="${CF_ENVIRONMENT_NAME:=cf}"
     local cert_file
+    local credhub_path
     if [[ -z "${BBL_STATE_DIR}" ]]; then
         set +e
         cert="$(openssl s_client -showcerts -connect "any.${CF_SYSTEM_DOMAIN}:443" </dev/null 2>/dev/null)"
         set -e
 
         cert_file="$(mktemp)"
+        credhub_path="/bosh-${CF_ENVIRONMENT_NAME}/cf/lb_cert"
 
         echo "${cert}" | openssl x509 > "${cert_file}"
     else
         cert_file="env/${BBL_STATE_DIR}/lb_certs/out/${BBL_STATE_DIR}.arp.cloudfoundry.org.crt"
+        credhub_path="/bosh-${BBL_STATE_DIR}/cf/lb_cert"
     fi
 
 
     credhub set \
-        --name "/bosh-${CF_ENVIRONMENT_NAME}/cf/lb_cert" \
+        --name "${credhub_path}" \
         --type certificate \
         --certificate "${cert_file}"
 }
