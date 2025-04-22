7bc6c50e0c6486ba66a8579b5720a1e94229130e
Amin Jamali <amin.jamali@broadcom.com>
2025-04-22 12:14:44 +0000

    Add BOSH_CREDS for bosh-upload-stemcell

--- shared/tasks/bosh-upload-stemcell/task.bash
+++ shared/tasks/bosh-upload-stemcell/task.bash
@@ -11,6 +11,7 @@ source "$THIS_FILE_DIR/../../../shared/helpers/bosh-helpers.bash"
 unset THIS_FILE_DIR
 
 function run(){
+    eval "${BOSH_CREDS}"
     bosh_target
     bosh upload-stemcell --fix  ${STEMCELL_URL}
 }
