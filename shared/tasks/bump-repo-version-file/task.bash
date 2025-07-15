9f4d2d786aa7d93a26d939a8d53c9dc51709cd62
Amin Jamali <amin.jamali@broadcom.com>
2025-07-15 12:50:38 +0000

    Add missing source for bosh-helpers

--- shared/tasks/bump-repo-version-file/task.bash
+++ shared/tasks/bump-repo-version-file/task.bash
@@ -6,6 +6,7 @@ set -o pipefail
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export TASK_NAME="$(basename $THIS_FILE_DIR)"
 source "$THIS_FILE_DIR/../../../shared/helpers/helpers.bash"
+source "$THIS_FILE_DIR/../../../shared/helpers/bosh-helpers.bash"
 source "$THIS_FILE_DIR/../../../shared/helpers/git-helpers.bash"
 unset THIS_FILE_DIR
 
