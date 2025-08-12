2049dccc199f28ca2646b7633b6186c06f47d671
Amelia Downs <amelia.downs@broadcom.com>
2025-08-12 15:24:15 +0000

    shep v2 changed their networks

--- shared/helpers/bosh-helpers.bash
+++ shared/helpers/bosh-helpers.bash
@@ -100,12 +100,10 @@ export CF_VM_TYPE=$(bosh int "${manifest}" --path /instance_groups/0/vm_type)"
     else
         echo  "export CF_STEMCELL_OS=$(bosh int "${manifest}" --path /stemcells/0/os)
 export CF_AZ=$(bosh int "${manifest}" --path /instance_groups/0/azs/0)
-export CF_NETWORK=$(bosh int "${cloud_config}" --path /networks/1/name)
-export CF_NETWORK_CIDR=$(bosh int "${cloud_config}" --path /networks/1/subnets/0/range)
-export CF_VM_TYPE=$(bosh int "${manifest}" --path /instance_groups/0/vm_type)
 export CF_LARGE_VM_TYPE=e2-standard-8
-export CF_NETWORK_SERVICES=$(bosh int "${cloud_config}" --path /networks/2/name)
-export CF_NETWORK_SERVICES_CIDR=$(bosh int "${cloud_config}" --path /networks/2/subnets/0/range)"
+export CF_NETWORK=$(bosh int "${cloud_config}" --path /networks/0/name)
+export CF_NETWORK_CIDR=$(bosh int "${cloud_config}" --path /networks/0/subnets/0/range)
+export CF_VM_TYPE=$(bosh int "${manifest}" --path /instance_groups/0/vm_type)"
     fi
 }
 
