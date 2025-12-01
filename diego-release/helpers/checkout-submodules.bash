e8749ebdedd7362ef4b8a146cdd1031c4b29a560
karthick udayakumar <karthick.udayakumar@broadcom.com>
2025-12-01 13:22:10 -0500

    Revert "pin guardian in diego-release"
    This reverts commit 7ce917cffb1954d89a6af5d73908b7e4067ba7e3.

--- diego-release/helpers/checkout-submodules.bash
+++ diego-release/helpers/checkout-submodules.bash
@@ -4,16 +4,11 @@ function run() {
   local repo_path=${1:?Provide a path to the repository}
 
   routing_api_version=70020201ed76f98cd276ad094b687cf73cc2dd04
-  guardian_version=ef3063efe1fe24dcbe1bc239489286c8e958c0b4
 
   pushd "${repo_path}" > /dev/null
   pushd src/code.cloudfoundry.org/routing-api > /dev/null
   echo "Checking out ${routing_api_version} routing-api"
   git checkout $routing_api_version
-  popd > /dev/null
-  pushd src/guardian > /dev/null
-  echo "Checking out ${guardian_version} guardian"
-  git checkout $guardian_version
   popd > /dev/null
   popd > /dev/null
 }
