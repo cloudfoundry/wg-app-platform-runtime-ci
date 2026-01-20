c4f3603b50c47dd16f129b1426d5482154b46d5d
Maria Shaldybin <maria.shaldybin@broadcom.com>
2026-01-20 18:10:45 +0000

    Revert "Support bosh deploy --var-file"
    This reverts commit 4a3e83be74d69823ff745805194a1ecc3950d536.

--- shared/tasks/bosh-deploy-manifest/task.bash
+++ shared/tasks/bosh-deploy-manifest/task.bash
@@ -61,18 +61,6 @@ function run(){
         arguments="${arguments} -l ${vs}"
     done
 
-    for vf in ${VAR_FILES:-}
-    do
-        if [[ ! -f "${vf}" ]]; then
-            if [[ ! -f "ops-files/${vf}" ]]; then
-                echo "Can't find var-file ${vf}"
-            else
-                vf="ops-files/${vf}"
-            fi
-        fi
-        arguments="${arguments} --var-file=${vf}"
-    done
-
     debug "bosh arguments for deploy: ${arguments}"
 
     echo "bosh -d ${DEPLOYMENT_NAME} deploy -n ${MANIFEST} --var=DEPLOYMENT_NAME=${DEPLOYMENT_NAME}${arguments}"
