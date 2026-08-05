148644f44a16966c710f8cf5de0b53e63526cb53
Amin Jamali <amin.jamali@broadcom.com>
2026-08-05 15:24:36 +0000

    Revert "Simplify sync_package by removing redundant src_dir and vendor_dir args"
    This reverts commit e539206137fef64962d69a1c60c0fd4eaf8cd044.

--- garden-runc-release/helpers/build-binaries.bash
+++ garden-runc-release/helpers/build-binaries.bash
@@ -149,7 +149,7 @@ function build_socket2me(){
     mkdir -p "${target}"
 
     pushd "$source" || exit
-    go build -o "${target}/run" code.cloudfoundry.org/guardian/cmd/socket2me
+    go build -o "${target}/run" .
     popd || exit
 
     cat > "${target}/run.bash" << EOF
@@ -167,7 +167,7 @@ function build_fake_runc_stderr(){
     mkdir -p "${target}"
 
     pushd "$source" || exit
-    go build -o "${target}/run" code.cloudfoundry.org/guardian/gqt/cmd/fake_runc_stderr
+    go build -o "${target}/run" .
     popd || exit
 
     cat > "${target}/run.bash" << EOF
@@ -185,13 +185,8 @@ function build_runc() {
     mkdir -p "${target}"
 
     pushd "$source" || exit
-    CGO_ENABLED=1 go build \
-        -trimpath \
-        -buildmode=pie \
-        -tags "seccomp urfave_cli_no_docs netgo osusergo" \
-        -ldflags "-X main.gitCommit= -linkmode external -extldflags -static-pie" \
-        -o "${target}/runc" \
-        github.com/opencontainers/runc
+    make BUILDTAGS='seccomp apparmor' static
+    mv runc "${target}"
     popd || exit
 
     cat > "${target}/run.bash" << EOF
@@ -203,14 +198,23 @@ function build_grootfs() {
     local source="${1?Provide source dir}"
     local target="${2?Provide target dir}"
 
+    if [[ "${WITH_MUSL:-no}" != "no" ]]; then
+        build_musl "$(echo ${source}|cut -d '/' -f1)" "${target}"
+        . "${target}/musl/run.bash"
+    fi
+
     local built_dir=$(basename "${target}")
     target="$target/grootfs"
     mkdir -p "${target}"
 
     pushd "$source" || exit
-    go build -tags cloudfoundry -o "${target}/grootfs" code.cloudfoundry.org/guardian/grootfs
-    go build -tags cloudfoundry -o "${target}/tardis" code.cloudfoundry.org/guardian/grootfs/store/filesystems/overlayxfs/tardis
-    chmod u+s "${target}/tardis"
+    make clean
+    if [  "${WITH_MUSL:-no}" != "no" ]; then
+        CC="${MUSL_BINARY}" STATIC_BINARY=true make
+    else
+        make
+    fi
+    make prefix="${target}" install
     popd || exit
 
     cat <<EOF > ${target}/grootfs-privileged.yml
@@ -291,7 +295,7 @@ function build_dadoo() {
     verify_go
 
     pushd "$source" || exit
-    go build -o "${target}/dadoo" code.cloudfoundry.org/guardian/cmd/dadoo
+    go build -o "${target}/dadoo" .
     popd || exit
 
     cat > "${target}/run.bash" << EOF
@@ -310,9 +314,9 @@ function build_idmapper() {
     verify_go
 
     pushd "$source" || exit
-    go build -o "${target}/newuidmap" code.cloudfoundry.org/guardian/idmapper/cmd/newuidmap
-    go build -o "${target}/newgidmap" code.cloudfoundry.org/guardian/idmapper/cmd/newgidmap
-    go build -o "${target}/maximus" code.cloudfoundry.org/guardian/idmapper/cmd/maximus
+    go build -o "${target}/newuidmap" ./cmd/newuidmap
+    go build -o "${target}/newgidmap" ./cmd/newgidmap
+    go build -o "${target}/maximus" ./cmd/maximus
     popd || exit
 
     cat > "${target}/run.bash" << EOF
--- garden-runc-release/helpers/build-binaries.ps1
+++ garden-runc-release/helpers/build-binaries.ps1
@@ -19,7 +19,7 @@ function Build-Winit
         New-Item -ItemType Directory -Force -Path "$Target"
 
         Push-Location "$Source"
-        go.exe build -o "$Target\run.exe" code.cloudfoundry.org/guardian/cmd/winit
+        go.exe build -o "$Target\run.exe" main.go
         if ($LastExitCode -ne 0) {
             exit 1
         }
@@ -49,7 +49,7 @@ function Build-Gdn
 
         Push-Location "$Source"
 
-        go.exe build -o "$Target\run.exe" -tags "hnsAcls windows" code.cloudfoundry.org/guardian/cmd/gdn
+        go.exe build -o "$Target\run.exe" -tags "hnsAcls" .
         if ($LastExitCode -ne 0) {
             exit 1
         }
